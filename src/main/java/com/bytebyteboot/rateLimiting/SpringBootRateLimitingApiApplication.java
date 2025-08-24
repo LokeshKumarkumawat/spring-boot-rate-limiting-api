13e5a88fdadae3ebdf879273a1e7a239e01360d7
lokesh <lokeshkumargithub@gmail.com>
2025-08-24 11:29:21 +0530

    INITIAL

---
+++ src/main/java/com/bytebyteboot/rateLimiting/SpringBootRateLimitingApiApplication.java
@@ -0,0 +1,13 @@
+package com.bytebyteboot.rateLimiting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRateLimitingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRateLimitingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bytebyteboot/rateLimiting/SpringBootRateLimitingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bytebyteboot.rateLimiting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRateLimitingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
